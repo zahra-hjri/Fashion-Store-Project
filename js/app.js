00c0eaea7075de8fbca01d90c1c8424f2a0c1c6d
Zahra Hojabri <zhojabri@ymail.com>
2023-01-23 19:37:33 +0330

    responsive header

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
-const headerBarsBtn = document.querySelector(".header__bars");
-const nav = document.querySelector("nav");
-const btnComment = document.querySelector(".fa-comment");
-headerBarsBtn.addEventListener("click", function () {
+const barsBtn = document.querySelector(".bars-btn");
+const mainMenu = document.querySelector(".main");
+const nav = document.querySelector(".responsive");
+const closeBtn = document.querySelector(".close-btn");
+const line = document.querySelector(".line");
+const headerIcons = document.querySelector(".header__icons");
+const formElem = document.querySelector(".form-elem");
+barsBtn.addEventListener("click", function () {
   nav.style.display = "flex";
-  headerBarsBtn.style.display = "none";
-  btnComment.style.display = "flex";
+  nav.style.justifyContent = "center";
+  nav.style.flexDirection = "column";
+  closeBtn.style.display = "block";
+  barsBtn.style.display = "none";
+});
+
+closeBtn.addEventListener("click", function () {
+  nav.style.display = "none";
+  barsBtn.style.display = "flex";
+  closeBtn.style.display = "none";
+  //   // headerIcons.style.display = "flex";
+  //   formElem.style.display = "none";
+  line.style.display = "none";
 });
